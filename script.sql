daf0dba5c4ebff5c65f7019fc3c873e82049b71e
cwilliam956 <swilliam956@gmail.com>
2023-03-13 21:46:43 -0300

    mudei

--- script.sql
+++ script.sql
@@ -4,37 +4,23 @@ USE seguradora;
 
 CREATE TABLE Clientes (
   CodCliente INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  Nome VARCHAR(45) NOT NULL,
-  CPF VARCHAR(45) NOT NULL UNIQUE,
-  Sexo VARCHAR(20),
-  Estado VARCHAR(45),
-  Cidade VARCHAR(45) DEFAULT 'Itapira',
-  Bairro VARCHAR(45),
-  Numero VARCHAR(45),
-  Rua VARCHAR(45),
-  TelefoneCelular VARCHAR(45) NOT NULL UNIQUE,
-  Cidade VARCHAR(45) DEFAULT 'Itapira'
+  Nome VARCHAR(100) NOT NULL,
+  CPF VARCHAR(11) NOT NULL UNIQUE,
+  TelefoneCelular VARCHAR(15) NOT NULL UNIQUE,
+  Cidade VARCHAR(50) DEFAULT 'Itapira'
 );
 
 CREATE TABLE Carro (
   CodCarro INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  Marca VARCHAR(45) NOT NULL,
-  Placa VARCHAR(45),
-  Modelo VARCHAR(45) NOT NULL,
+  Marca VARCHAR(50) NOT NULL,
+  Modelo VARCHAR(50) NOT NULL,
   Ano INT NOT NULL
-  Chassi VARCHAR (45),
-  Cor VARCHAR (45),
 );
 
 CREATE TABLE Apólice (
   CodApolice INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-  CodApolice INT PRIMARY KEY,
-  ValorCobertura DECIMAL NOT NULL,
-  ValorFranquia DECIMAL NOT NULL,
-  DataInicioVigencia DATE NOT NULL,
-  DataFimVigencia DATE NOT NULL,
-  Cliente_CodCliente INT NOT NULL,
-  Carro_CodCarro INT NOT NULL,
+  CodCliente INT NOT NULL,
+  CodCarro INT NOT NULL,
   DataInicioVigencia DATE NOT NULL CHECK (DataInicioVigencia > GETDATE()),
   CONSTRAINT FK_Cliente_Apolice FOREIGN KEY (CodCliente) REFERENCES Clientes(CodCliente),
   CONSTRAINT FK_Carro_Apolice FOREIGN KEY (CodCarro) REFERENCES Carro(CodCarro)
@@ -43,14 +29,9 @@ CREATE TABLE Apólice (
 CREATE TABLE Sinistro (
   CodApolice INT NOT NULL,
   CodCarro INT NOT NULL,
-  HoraSinistro INT,
-  DataSinistro DATE,
-  LocalSinistro VARCHAR(45),
-  Condutor VARCHAR(45),
   PRIMARY KEY (CodApolice, CodCarro),
   CONSTRAINT FK_Apolice_Sinistro FOREIGN KEY (CodApolice) REFERENCES Apólice(CodApolice),
   CONSTRAINT FK_Carro_Sinistro FOREIGN KEY (CodCarro) REFERENCES Carro(CodCarro)
 );
-
 
 DROP TABLE Clientes;
